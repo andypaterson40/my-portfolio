257eb0648579bc7a589e51758e563e94cfb5a074
Andrew Paterson <andypaterson40@gmail.com>
2018-08-09 15:10:45 +0100

    updating lambda function with exceptions

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,22 +5,29 @@ import mimetypes
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic("arn:aws:sns:eu-west-1:276935992059:deployPortfolioTopic")
 
-    portfolio_bucket = s3.Bucket('portfolio.awpitconsulting.com')
-    build_bucket = s3.Bucket('portfoliobuild.awpitconsulting.com')
+    try:
+        s3 = boto3.resource('s3')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.awpitconsulting.com')
+        build_bucket = s3.Bucket('portfoliobuild.awpitconsulting.com')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            mime_type = mimetypes.guess_type(nm)[0]
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': str(mime_type)})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print 'Job Done!'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                mime_type = mimetypes.guess_type(nm)[0]
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': str(mime_type)})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
+        print 'Job Done!'
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully")
+        raise
     return 'Hello world'
